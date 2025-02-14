e7a55e84022e0ec268366dfdc45be9e12f0e459b
gusevskiy <vlgu@mail.ru>
2025-02-14 20:16:05 +0300

    добавил проверку email регуляркой

--- mailing/product/forms.py
+++ mailing/product/forms.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django import forms
+import re
 from .models import Mailing, MailingEmails
 
 class MailingForm(forms.ModelForm):
@@ -7,12 +8,23 @@ class MailingForm(forms.ModelForm):
     
     class Meta:
         model = Mailing
+        help_text = {
+            'header_email': 'Заголовок/Приветствие',
+            'body_text': 'Текст письма',
+            'sender_email': 'Отправитель данного письма'
+        }
         fields = ['header_email', 'body_text', 'sender_email']
 
     def clean_emails(self):
+        """
+        Валидатор (вызывается автоматически)
+        проверяет соответствие кождого элемента списка
+        email адресу.
+        """
         # Преобразуем строку с email-ами в список
+        pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         emails = self.cleaned_data['emails']
-        email_list = [email.strip() for email in emails.split(',')]
+        email_list = [email.strip() for email in emails.split(',') if re.match(pattern, email)]
         return email_list
     
     def save(self, commit=True):
@@ -20,7 +32,7 @@ class MailingForm(forms.ModelForm):
         if commit:
             mailing.save()
         
-        # Сохраняем каждый email как отдельную запись в MailingLog
+        # Сохраняем каждый email как отдельную запись в MailingEmails
         email_list = self.cleaned_data['emails']
         for email in email_list:
             MailingEmails.objects.create(mailing=mailing, email=email)
